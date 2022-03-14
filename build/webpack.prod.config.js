fb30741f00df1c9833ea4671d3c697d611b19c4b
dongwenjun5 <dongwenjun5@hikvision.com>
2022-03-14 19:00:55 +0800

    add product config

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -2,12 +2,14 @@ const webpackBaseConfig = require('./webpack.base.config.js')
 const merge = require('webpack-merge')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const utils = require('./utils')
+
 
 module.exports = merge(webpackBaseConfig, {
   mode: "production",
   output: {
-    filename: '[name].js',
-    chunkFilename: '[name].chunk.js'
+    filename: utils.assetsPath('/js/[name].[chunkhash].js'),
+    chunkFilename: utils.assetsPath('/js/[id].[chunkhash].js')
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -23,4 +25,24 @@ module.exports = merge(webpackBaseConfig, {
       parallel: true
     }),
   ],
+  optimization: {
+    splitChunks: { // 将node_modules 中的css js 从main.js/css 中分离生成vendors 文件
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'initial',
+        },
+        'async-vendors': {
+          test: /[\\/]node_modules[\\/]/,
+          minChunks: 2,
+          chunks: 'async',
+          name: 'async-vendors'
+        }
+      },
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime~${entrypoint.name}`,
+    },
+  },
 })
